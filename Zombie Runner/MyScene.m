8711e09f4343e5295162b3f52541b97f61e85e5c
cmmatzat <corey@thematzats.com>
2014-04-23 16:06:25 -0500

    Started working on resetGame function

--- Zombie Runner/MyScene.m
+++ Zombie Runner/MyScene.m
@@ -1,4 +1,3 @@
-//
 //  MyScene.m
 //  Zombie Runner
 //
@@ -12,26 +11,13 @@
 
 @implementation MyScene
 
--(id)initWithSize:(CGSize)size {    
-    if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
+-(id)initWithSize:(CGSize)size
+{
+    if (self = [super initWithSize:size])
+    {
+        self.backgroundColor = [SKColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1.0];
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [[Zombie alloc] initZombieForParent:self];
-        [[Zombie alloc] initZombieForParent:self];
-        [[Zombie alloc] initZombieForParent:self];
-        
-        [[Player alloc] initPlayerForParent:self];
-        
-        [self addChild:myLabel];
+        [self resetGame];
     }
     return self;
 }
@@ -56,6 +42,20 @@
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+}
+
+-(void)resetGame
+{
+    for (SKNode *child in self.children)
+    {
+        [child removeFromParent];
+    }
+    
+    [[Zombie alloc] initZombieForParent:self];
+    [[Zombie alloc] initZombieForParent:self];
+    [[Zombie alloc] initZombieForParent:self];
+    
+    [[Player alloc] initPlayerForParent:self];
 }
 
 @end
