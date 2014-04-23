a3c663c50cffe1ac4141430ae254bea80046a80b
cmmatzat <corey@thematzats.com>
2014-04-23 15:17:55 -0500

    Added Zombie class to project

---
+++ Zombie Runner/Zombie.h
@@ -0,0 +1,15 @@
+//
+//  Zombie.h
+//  Zombie Runner
+//
+//  Created by Corey Matzat on 4/23/14.
+//  Copyright (c) 2014 Corey Matzat. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Zombie : SKSpriteNode
+
+-(instancetype)initZombieForParent:(SKScene*)parent;
+
+@end
